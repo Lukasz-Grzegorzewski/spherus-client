38623df6995fe1429289cf8c9534aebfb0284d0e
lukasz <grzegorzewski.luk@gmail.com>
2024-10-08 22:51:58 +0200

    github actions : build UI => copy dist to VPS => update docker-compose.yml => relunch docker compose

--- src/components/homeDisplay/HomeDisplay.jsx
+++ src/components/homeDisplay/HomeDisplay.jsx
@@ -1,41 +1,41 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import Section from '../section/Section';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Section from "../section/Section";
 
 function HomeDisplay() {
-  const [currentHome, setCurrentHome] = useState({});
+	const [currentHome, setCurrentHome] = useState([]);
 
-  useEffect(() => {
-    const getHome = () => {
-      axios
-        .get(`${import.meta.env.VITE_URL_SPHERUS_API}/api/home`)
-        .then((res) => {
-          setCurrentHome(res.data);
-        })
-        .catch((err) => console.error(err));
-    };
-    getHome();
-  }, []);
+	useEffect(() => {
+		const getHome = () => {
+			axios
+				.get(`${import.meta.env.VITE_URL_SPHERUS_API}/api/home`)
+				.then((res) => {
+					if (res.data && Array.isArray(res.data)) setCurrentHome(res.data);
+				})
+				.catch((err) => console.error(err));
+		};
+		getHome();
+	}, []);
 
-  return (
-    <div className='homeDisplay'>
-      {currentHome.length >= 1 && (
-        <div>
-          {currentHome.map((infos) => {
-            return (
-              <div key={infos.id}>
-                <Section
-                  type={infos.type}
-                  id={infos.id}
-                  idLink={infos.idLink}
-                />
-              </div>
-            );
-          })}
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div className="homeDisplay">
+			{currentHome.length >= 1 && (
+				<div>
+					{currentHome.map((infos) => {
+						return (
+							<div key={infos.id}>
+								<Section
+									type={infos.type}
+									id={infos.id}
+									idLink={infos.idLink}
+								/>
+							</div>
+						);
+					})}
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default HomeDisplay;
